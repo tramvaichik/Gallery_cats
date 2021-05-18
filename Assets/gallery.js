1fe8ad32089b942c0f0f07c8f3446dc4c864747b
tramvaichik <arsee@list.ru>
2021-05-18 19:48:11 +0300

    галерея js

---
+++ Assets/gallery.js
@@ -0,0 +1,9 @@
+ function changeImgNext() {
+     let catImage = document.getElementById("prince");
+     catImage.src = "/Assets/images/cat_countess.jpeg";
+ }
+
+ function changeImgBack() {
+     let catImage = document.getElementById("prince");
+     catImage.src = "/Assets/images/prince_of_cats.jpeg";
+ }
